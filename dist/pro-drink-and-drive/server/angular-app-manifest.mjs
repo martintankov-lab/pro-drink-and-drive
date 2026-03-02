6a1a07654295c7ce956bed7b6fd3035f6772fb96
Alen-Nikolov <alennikolov95@gmail.com>
2026-03-02 14:39:53 +0200

    new build

--- dist/pro-drink-and-drive/server/angular-app-manifest.mjs
+++ dist/pro-drink-and-drive/server/angular-app-manifest.mjs
@@ -10,12 +10,12 @@ export default {
     "chunk-LHQLGZWN.js"
   ],
   "src/app/pages/home/home.component.ts": [
-    "chunk-C2C4EBAA.js"
+    "chunk-AUJXNAIR.js"
   ]
 },
   assets: {
-    'index.csr.html': {size: 34146, hash: '079ba249cd0b8eaa08beb5effb278d8be1ff4da7b45e6c34502b884807a68a76', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 27573, hash: '0187ab66a157062e63ced1ab4d039e7f51466052c73bc8a8d2f170d2f9f1bf9d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 34146, hash: 'ff1b7f5bb1950f478766190d05d37dee6a5d7f5810c8c3c460f9a4b039ccd2d8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 27573, hash: 'e52b759a61a09d4c3e65059a051de06aa1a911eb72221be75cfbe8a6dd1d6875', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-ZJTXBPAZ.css': {size: 7126, hash: 'AYMTBwra9bM', text: () => import('./assets-chunks/styles-ZJTXBPAZ_css.mjs').then(m => m.default)}
   },
 };
