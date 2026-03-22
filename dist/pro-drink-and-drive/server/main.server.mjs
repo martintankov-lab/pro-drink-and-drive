3444834e4cbdf262c629aead0f42f3b5499f2781
Alen-Nikolov <alennikolov95@gmail.com>
2026-03-22 14:45:03 +0200

    removed dist

--- dist/pro-drink-and-drive/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{e,f as t,h as o,i as p,j as m}from"./chunk-TUPBQW54.mjs";import"./chunk-IJY7ID4U.mjs";import{Sb as r}from"./chunk-APJ26YMW.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
